1235dbce08bdae0a7481b8bbb09299644a7beda7
Dries Grobler Jr <driesjr@gmail.com>
2017-08-06 22:47:32 +0200

    Spelling and font
    Corrected spelling of You to Your and added a purple Arial font

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
 
     <body>
 <div align="center">
+              <font face="arial" color="purple">
                 <h1>Heybra</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
                 <div align="center"><img src="https://s3.eu-central-1.amazonaws.com/heybra-learning/ferrari_2017.jpg"></div>
-                <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>Your ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+              </font>
         </div>
     </body>
 
